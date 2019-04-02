359bdd73dffe6e22e55df64ea7e219b9440f6155
Nick Puetz <Napez702@hotmail.com>
2019-04-02 10:03:33 -0400

    stop

--- EmployeeRecords/Form1.cs
+++ EmployeeRecords/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,7 +20,10 @@ namespace EmployeeRecords
         }
 
         private void addButton_Click(object sender, EventArgs e)
-        { 
+        {
+            Employee yeet = new Employee(idInput.Text, fnInput.Text, lnInput.Text, dateInput.Text, salaryInput.Text);
+            employeeDB.Add(yeet);
+            outputLabel.Text = "employee added";
             ClearLabels();
         }
 
@@ -31,7 +34,14 @@ namespace EmployeeRecords
 
         private void listButton_Click(object sender, EventArgs e)
         {
-
+            outputLabel.Text = "";
+            foreach (Employee p in employeeDB)
+            {
+                outputLabel.Text += "Employee ID: " + p.id;
+                outputLabel.Text += "\nName: " + p.firstName + " "+  p.lastName;
+                outputLabel.Text += "\nSalary: " + p.salary;
+                outputLabel.Text += "\nDate Started: " + p.date + "\n";
+            }
         }
 
         private void ClearLabels()
@@ -45,7 +55,7 @@ namespace EmployeeRecords
 
         private void mainForm_FormClosed(object sender, FormClosedEventArgs e)
         {
-
+            saveDB();
         }
 
         public void loadDB()
@@ -55,7 +65,18 @@ namespace EmployeeRecords
 
         public void saveDB()
         {
+            XmlWriter writer = XmlWriter.Create("Resources/EmployeeData.xml", null);
+            writer.WriteStartElement("Employee");
 
+            foreach(Employee p in employeeDB)
+            {
+                writer.WriteElementString("ID", p.id);
+                writer.WriteElementString("FirstName", p.firstName);
+                writer.WriteElementString("LastName", p.lastName);
+                writer.WriteElementString("Salary", p.salary);
+                writer.WriteElementString("StartDate", p.date);
+            }
+            
         }
     }
 }
